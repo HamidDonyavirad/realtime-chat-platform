dacb27c1901b688b958dcd31be03bcc09f765ba3
Hamid <donyavirad76@gmail.com>
2026-08-18 18:05:21 +0330

    feat(auth-service):Initializing the main file and creating the config file and project configuration codes

---
+++ services/auth-service/app/core/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, settingsConfigDict
+
+class Settings(BaseSettings):
+    database_uri: str
+
+    jwt_secret_key: str
+    jwt_algorithm: str = "HS256"
+
+    access_token_expire_minutes: int = 30
+    refresh_token_expire_days: int = 7
+
+    model_config = settingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra = "ignore",
+    )
+
+settings = Settings()
