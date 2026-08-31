0c55512dd897c817ca8da515601d48be5c61d9c6
Hamid <donyavirad76@gmail.com>
2026-08-31 08:52:51 +0330

    feat(auth-service):add JWT access token creation

---
+++ services/auth-service/app/security/jwt.py
@@ -0,0 +1,19 @@
+from datetime import datetime,timezone,timedelta
+from jose import jwt
+
+from app.core.config import settings
+
+
+def create_access_token(user_id:str) ->str:
+    expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload = {
+        "sub": user_id,
+        "type":"access",
+        "exp": expire
+    }
+    return jwt.encode(
+        payload,
+        settings.jwt_secret_key,
+        algorithm=settings.jwt_algorithm
+    )
+
