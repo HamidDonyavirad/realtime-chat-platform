947dfd6ef745efa196134e9853f609b327ba8fea
Hamid <donyavirad76@gmail.com>
2026-09-12 18:13:46 +0330

    feat(auth-service):implement refresh token repository operations

---
+++ services/auth-service/app/repositories/refresh_token_repository.py
@@ -0,0 +1,29 @@
+
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.refresh_token import RefreshToken
+
+
+class RefreshTokenRepository:
+    def __init__(self, db:AsyncSession):
+        self.db = db
+
+    async def create (self,refresh_token:RefreshToken) -> RefreshToken:
+        self.db.add(refresh_token)
+        await self.db.commit()
+        await self.db.refresh(refresh_token)
+        return refresh_token
+
+    async def get_by_hash(self,token_hash:str) -> RefreshToken | None:
+        result = await self.db.execute(
+            select(RefreshToken).where(RefreshToken.token_hash == token_hash)
+        )
+        return result.scalar_one_or_none()
+
+    async def revorke (self,refresh_token:RefreshToken) ->  None:
+        refresh_token.is_revorked = True
+        await self.db.commit()
+
+
